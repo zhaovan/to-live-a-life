61486219166cc679e75035a9dc792d319d41b8af
zhaovan <ivanzhao8@gmail.com>
2023-12-11 23:05:27 -0800

    fix

---
+++ src/app/constants.ts
@@ -0,0 +1,136 @@
+export const story = [
+  {
+    age: 0,
+    textBlock: [
+      {
+        text: "you are born from [INSERT]",
+        textOptions: ["a c section", "force", "an open question"],
+      },
+    ],
+  },
+  {
+    age: 10,
+    textBlock: [
+      {
+        text: "you [INSERT]",
+        textOptions: [
+          "speak your first word",
+          "take your first steps",
+          "hold your father",
+          "are rushed to the hospital suffering from an incurable disease",
+        ],
+      },
+    ],
+  },
+  {
+    age: 20,
+    textBlock: [
+      {
+        text: "school slaps you awake for the first time. you bring out your lunchbox filled with [INSERT]. you nibble a corner and it reminds you that there are worlds within worlds.",
+        textOptions: [
+          "lunchables",
+          "steamed fish and rice",
+          "nothing",
+          "a peanut butter and jelly sandwich",
+        ],
+      },
+    ],
+  },
+  {
+    age: 30,
+    textBlock: [
+      {
+        text: "if you were to question [INSERT], what would you ask?",
+        textOptions: ["god", "your father", "the world", "this life"],
+      },
+    ],
+    buttonOptions: [
+      "to be reborn",
+      "to love, to be loved",
+      "for mercy and understanding",
+    ],
+  },
+  {
+    age: 40,
+    textBlock: [
+      {
+        text: "a [INSERT] ",
+        textOptions: ["boy", "girl", "bird"],
+      },
+      {
+        text: "comes up and asks for the [INSERT] ",
+        textOptions: ["cards", "lunch", "life"],
+      },
+      {
+        text: "in your hands. your father entrusted these to you when he [INSERT] last week.",
+        textOptions: ["passed", "went on vacation", "held you", "kissed you"],
+      },
+    ],
+  },
+  {
+    age: 50,
+    textBlock: [
+      {
+        text: "[INSERT]",
+        textOptions: [
+          "nestled in the tree-lined neighborhood, mixed with fresh fruit and asphalt lies a small house. a single light dimly lights up a bedroom. inside there is a desk, a queen bed, two dresses. in the bathroom, light humming can be heard. on the carpet of the room next door, a chest of toys overflows, spilling out onto the floor.",
+          "you step out into the porch. the wind seeps into the nooks of your hair as you pull her aside by the waist. around you, there are peach and olive farms. the supple fruit dangles languidly. pit to flesh. the tongue lusts for nectar. she provides. a herd of broken peaches gush on the ground",
+          "a pot wafts with garlicky want. your father never told you how he broke down roots, to crush the cloves under metal skin. how he used to forage for mushrooms in the nearby woods. how the vines clawed into his feet",
+        ],
+      },
+    ],
+  },
+  {
+    age: 60,
+    textBlock: [
+      {
+        text: "the [INSERT] crack happens. it had been happening over the years, but now it’s louder, pulsing",
+        textOptions: ["first", "third", "fifth", "one hundredth"],
+      },
+    ],
+  },
+  {
+    age: 70,
+    textBlock: [
+      {
+        text: "placeholder",
+        textOptions: ["placeholder"],
+      },
+    ],
+  },
+  {
+    age: 80,
+    textBlock: [
+      {
+        text: "somedays, a little [INSERT] matters",
+        textOptions: ["card", "lunch", "life"],
+      },
+    ],
+  },
+  {
+    age: 90,
+    textBlock: [
+      {
+        text: "everyone you love has died. you too, shall pass. their memories live within you.",
+        textOptions: [],
+      },
+    ],
+  },
+  {
+    age: 100,
+    textBlock: [
+      {
+        text: "when he turned this age, he wished that he could be born again. what did he mean by that?",
+        textOptions: [],
+      },
+    ],
+  },
+];
+
+export const endStateStory = [
+  "in death, the second law of thermodynamics is broken",
+  "if humans are bundles of complexity, then what is death?",
+  "a breakage of entropy",
+  "there is no energy in scattered stardust",
+  "only stillness",
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,150 +1,20 @@
 "use client";
 import { useState, useRef } from "react";
 import Button from "./components/Button";
+import { story, endStateStory } from "./constants";
 
-const story = [
-  {
-    age: 0,
-    textBlock: [
-      {
-        text: "you are born from [INSERT]",
-        textOptions: ["a c section", "force", "an open question"],
-      },
-    ],
-  },
-  {
-    age: 10,
-    textBlock: [
-      {
-        text: "you [INSERT]",
-        textOptions: [
-          "speak your first word",
-          "take your first steps",
-          "hold your father",
-          "are rushed to the hospital suffering from an incurable disease",
-        ],
-      },
-    ],
-  },
-  {
-    age: 20,
-    textBlock: [
-      {
-        text: "school slaps you awake for the first time. you bring out your lunchbox filled with [INSERT]. you nibble a corner and it reminds you that there are worlds within worlds.",
-        textOptions: [
-          "lunchables",
-          "steamed fish and rice",
-          "nothing",
-          "a peanut butter and jelly sandwich",
-        ],
-      },
-    ],
-  },
-  {
-    age: 30,
-    textBlock: [
-      {
-        text: "if you were to question [INSERT], what would you ask?",
-        textOptions: ["god", "your father", "the world", "this life"],
-      },
-    ],
-    buttonOptions: [
-      "to be reborn",
-      "to love, to be loved",
-      "for mercy and understanding",
-    ],
-  },
-  {
-    age: 40,
-    textBlock: [
-      {
-        text: "a [INSERT] ",
-        textOptions: ["boy", "girl", "bird"],
-      },
-      {
-        text: "comes up and asks for the [INSERT] ",
-        textOptions: ["cards", "lunch", "life"],
-      },
-      {
-        text: "in your hands. your father entrusted these to you when he [INSERT] last week.",
-        textOptions: ["passed", "went on vacation", "held you", "kissed you"],
-      },
-    ],
-  },
-  {
-    age: 50,
-    textBlock: [
-      {
-        text: "[INSERT]",
-        textOptions: [
-          "nestled in the tree-lined neighborhood, mixed with fresh fruit and asphalt lies a small house. a single light dimly lights up a bedroom. inside there is a desk, a queen bed, two dresses. in the bathroom, light humming can be heard. on the carpet of the room next door, a chest of toys overflows, spilling out onto the floor.",
-          "you step out into the porch. the wind seeps into the nooks of your hair as you pull her aside by the waist. around you, there are peach and olive farms. the supple fruit dangles languidly. pit to flesh. the tongue lusts for nectar. she provides. a herd of broken peaches gush on the ground",
-          "a pot wafts with garlicky want. your father never told you how he broke down roots, to crush the cloves under metal skin. how he used to forage for mushrooms in the nearby woods. how the vines clawed into his feet",
-        ],
-      },
-    ],
-  },
-  {
-    age: 60,
-    textBlock: [
-      {
-        text: "the [INSERT] crack happens. it had been happening over the years, but now it’s louder, pulsing",
-        textOptions: ["first", "third", "fifth", "one hundredth"],
-      },
-    ],
-  },
-  {
-    age: 70,
-    textBlock: [
-      {
-        text: "placeholder",
-        textOptions: ["placeholder"],
-      },
-    ],
-  },
-  {
-    age: 80,
-    textBlock: [
-      {
-        text: "somedays, a little [INSERT] matters",
-        textOptions: ["card", "lunch", "life"],
-      },
-    ],
-  },
-  {
-    age: 90,
-    textBlock: [
-      {
-        text: "everyone you love has died. you too, shall pass. their memories live within you.",
-        textOptions: [],
-      },
-    ],
-  },
-  {
-    age: 100,
-    textBlock: [
-      {
-        text: "when he turned this age, he wished that he could be born again. what did he mean by that?",
-        textOptions: [],
-      },
-    ],
-  },
-];
-
-const endStateStory = [
-  "in death, the second law of thermodynamics is broken",
-  "if humans are bundles of complexity, then what is death?",
-  "a breakage of entropy",
-  "there is no energy in scattered stardust",
-  "only stillness",
-];
+enum GameState {
+  start,
+  journey,
+  end,
+}
 
 export default function Home() {
   const [step, setStep] = useState(0);
   const [journey, setJourney] = useState<string[]>([]);
 
   const textRef = useRef<HTMLParagraphElement>(null);
-  const [endState, setEndState] = useState(false);
+  const [gameState, setGameState] = useState(GameState.start);
   const [endStateStep, setEndStateStep] = useState(0);
 
   function handleClick() {
@@ -153,7 +23,7 @@ export default function Home() {
       setJourney([...journey, text]);
     }
     if (step === story.length - 1) {
-      setEndState(true);
+      setGameState(GameState.end);
     }
     setStep(Math.min(step + 1, story.length - 1));
   }
@@ -165,14 +35,15 @@ export default function Home() {
     <div className="h-screen w-full flex gap-6 p-6 font-facade bg-gray-300">
       <div className="w-[66vw] border border-black p-6 flex items-center justify-center">
         <div className="w-1/2 flex flex-col gap-2">
-          {endState ? (
+          {gameState === GameState.start && (
             <>
               <p>{endStateStory[endStateStep]}</p>
               <Button handleClick={() => setEndStateStep(endStateStep + 1)}>
                 ...
               </Button>
             </>
-          ) : (
+          )}
+          {gameState === GameState.journey && (
             <>
               <h1 className="text-lg">you are {randomAge} years old</h1>
               <p className="font-serif" ref={textRef}>
@@ -185,7 +56,11 @@ export default function Home() {
                     textBlock.textOptions[randomOption]
                   );
 
-                  return <span key={idx}>{lifeChoice}</span>;
+                  return (
+                    <span key={idx} suppressHydrationWarning>
+                      {lifeChoice}
+                    </span>
+                  );
                 })}
               </p>
 
@@ -203,9 +78,17 @@ export default function Home() {
               )}
             </>
           )}
+          {gameState === GameState.end && (
+            <>
+              <p>{endStateStory[endStateStep]}</p>
+              <Button handleClick={() => setEndStateStep(endStateStep + 1)}>
+                ...
+              </Button>
+            </>
+          )}
         </div>
       </div>
-      <div className="border border-gray-700 w-[33vw] p-6 flex flex-col gap-4">
+      <div className="border border-gray-700 overflow-scroll w-[33vw] p-6 flex flex-col gap-4">
         <h1 className="text-2xl">your journey</h1>
         <div className="flex flex-col gap-2">
           {journey.map((journeyItem, idx) => {
